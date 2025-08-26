b00c7c626dc378f8ddce531a7f3a2893d96aee20
--replace-all <rmeji3@uic.edu>
2025-08-26 03:09:12 -0500

    feat: add analytics component with graph visualization
    - Added a new Analytics component to display weekly views.
    - Created a Graph component using MUI's LineChart for data visualization.
    - Updated the Home page to include the Analytics component with sample data.
    - Added necessary dependencies for MUI and Emotion for styling.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+//import components
+import Analytics from "../components/analytics";
+
+const viewsData = [10, 20, 30, 11, 41, 100, 110];
 const Home: React.FC = () => {
     return (
-        <main className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-            <h1 className="text-3xl text-black font-bold mb-4">Welcome to the Home Page</h1>
+        <main className="min-h-screen flex flex-col items-center bg-gray-100">
+            {/* gradient circles */}
+            <div className="absolute top-40 -left-32 w-[400px] h-[400px] bg-gradient-to-br from-purple-500 to-blue-500 rounded-full opacity-40 blur-2xl z-0"  aria-hidden="true" role="presentation"/>
+            <div className="absolute top-20 left-3/4 w-[300px] h-[300px] bg-gradient-to-tr from-blue-400 to-purple-400 rounded-full opacity-30 blur-2xl z-0"  aria-hidden="true" role="presentation"/>
+            <div className="absolute bottom-0 right-0 w-[250px] h-[250px] bg-gradient-to-tl from-purple-300 to-blue-300 rounded-full opacity-20 blur-2xl z-0" aria-hidden="true" role="presentation"/>
+
+            {/* Analytics card */}
+            <Analytics weeklyViews={viewsData}/>
+            
         </main>
     );
 };
